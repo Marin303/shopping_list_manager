0fdfdbd5095d45d71d85c63424b24cab60fe87c3
Marin <marin.muktic3@gmail.com>
2023-11-18 11:00:35 +0100

    react-router install

--- frontend/src/Page/Layout/Layout.jsx
+++ frontend/src/Page/Layout/Layout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ShoppingList from "../../Components/ShoppingList/ShoppingList";
 import Analytics from "../../Components/Analytics/Analytics";
+import "./layout.scss";
+import { Route, Routes, Navigate } from "react-router-dom";
+
 const Layout = () => {
+  
   return (
     <div className="layout_container">
-      <ShoppingList />
-      <Analytics />
+      <header className="layout_container_header">
+        <Routes>
+          <Route path="/" element={<Navigate to="/shopping-list" />} />
+          <Route path="/shopping-list" element={<ShoppingList />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Routes>
+      </header>
     </div>
   );
 };
