f4070800d62bc26cd6d972bd613a284580f3d315
Marin <marin.muktic3@gmail.com>
2023-11-22 22:38:09 +0100

    chart-js created

--- frontend/src/Components/Analytics/Analytics.jsx
+++ frontend/src/Components/Analytics/Analytics.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./analytics.scss";
+import { Pie } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+
+Chart.register(ArcElement);
 
 const Analytics = () => {
   const [products, setProducts] = useState([]);
@@ -30,22 +34,21 @@ const Analytics = () => {
   };
   // console.log(products);
   // console.log("apparel", products['Apparel']);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const convertDateToMonth = (dateString) => {
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-
     const dateParts = dateString.split("/");
     const monthIndex = parseInt(dateParts[0], 10) - 1; // 0-based is Months
 
@@ -66,45 +69,66 @@ const Analytics = () => {
     0
   );
 
+  const chartData = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
   return (
     <div className="analytics_container">
       <h4 className="header_title">Analytics</h4>
-      <select name="month" id="month" onChange={handleMonthChange}>
-        <option value="January">January</option>
-        <option value="February">February</option>
-        <option value="March">March</option>
-        <option value="April">April</option>
-        <option value="May">May</option>
-        <option value="June">June</option>
-        <option value="July">July</option>
-        <option value="August">August</option>
-        <option value="September">September</option>
-        <option value="October">October</option>
-        <option value="November">November</option>
-        <option value="December">December</option>
-      </select>
-      <select
-        name="product-category"
-        id="product-category"
-        onChange={handleCategoryChange}
-      >
-        <option value="Apparel">Apparel</option>
-        <option value="Footwear">Footwear</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Household">Household</option>
-        <option value="Technique">Technique</option>
-        <option value="Other">Other</option>
-      </select>
+      <div className="analytics_wrapper">
+        <select name="month" id="month" onChange={handleMonthChange}>
+          <option value="January">January</option>
+          <option value="February">February</option>
+          <option value="March">March</option>
+          <option value="April">April</option>
+          <option value="May">May</option>
+          <option value="June">June</option>
+          <option value="July">July</option>
+          <option value="August">August</option>
+          <option value="September">September</option>
+          <option value="October">October</option>
+          <option value="November">November</option>
+          <option value="December">December</option>
+        </select>
+        <select
+          name="product-category"
+          id="product-category"
+          onChange={handleCategoryChange}
+        >
+          <option value="Apparel">Apparel</option>
+          <option value="Footwear">Footwear</option>
+          <option value="Groceries">Groceries</option>
+          <option value="Household">Household</option>
+          <option value="Technique">Technique</option>
+          <option value="Other">Other</option>
+        </select>
 
-      <div>
-        {filteredProducts.map((product, index) => (
-          <div key={index}>
-            <p>Name: {product.name}</p>
-            <p>Price: {product.price}€</p>
-            <p>Date: {convertDateToMonth(product.date)}</p>
-          </div>
-        ))}
-        <p>Total sum: {totalSum.toFixed(2)}€</p>
+        <div>
+          {filteredProducts.map((product, index) => (
+            <div key={index}>
+              <p>Name: {product.name}</p>
+              <p>Price: {product.price}€</p>
+              <p>Date: {convertDateToMonth(product.date)}</p>
+            </div>
+          ))}
+          <p className="total-sum">Total sum: {totalSum.toFixed(2)}€</p>
+        </div>
+        <div className="chart">
+          <Pie data={chartData} />
+        </div>
       </div>
     </div>
   );
